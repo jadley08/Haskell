ffa4b8177e46693e35b8f4f053e600e7d59c70a6
adleypc <jadley08@gmail.com>
2019-11-07 07:52:10 -0500

    gross but working

--- Heap/Heap.hs
+++ Heap/Heap.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE MultiWayIf #-}
---import Data.List
---import Data.Bool
+
+--module Heap
+  --(Heap,elements,element,isHeap,insert,merge) where
+
 import Data.Maybe
 
 owise :: Bool
@@ -39,24 +41,65 @@ isHeap (Heap f e l r) = let l_e = peek l
                             (isNothing r_e ||
                              (f e (fromJust r_e)))
 
-insert :: (Heap v) -> v -> (Heap v)
-insert Empty v          = Empty
-insert (Heap f e l r) v =
+insert :: (Heap v) -> v -> (v -> v -> Bool) -> (Heap v)
+insert Empty v f          = (Heap f v Empty Empty)
+insert (Heap f e l r) v _ =
   let l_c    = size l
       r_c    = size r
       cmp_ve = (f v e)
-   in if | cmp_ve && (l_c < r_c) -> (Heap f v (insert l e) r)
-         | cmp_ve                -> (Heap f v l (insert r e))
-         | l_c <= r_c            -> (Heap f e (insert l v) r)
-         | owise                 -> (Heap f e l (insert r v))
+   in if | cmp_ve && (l_c < r_c) -> (Heap f v (insert l e f) r)
+         | cmp_ve                -> (Heap f v l (insert r e f))
+         | l_c < r_c             -> (Heap f e (insert l v f) r)
+         | owise                 -> (Heap f e l (insert r v f))
 
-merge :: (Heap v) -> (Heap v) -> (Heap v)
-merge h1 Empty          = h1
-merge Empty h2          = h2
-merge h1 (Heap _ e l r) = merge (merge (insert h1 e) l) r
+merge :: (Heap v) -> (Heap v) -> (v -> v -> Bool) -> (Heap v)
+merge h1 Empty _          = h1
+merge Empty h2 _          = h2
+merge h1 (Heap _ e l r) f = merge (merge (insert h1 e f) l f) r f
+
+fromList :: [v] -> (v -> v -> Bool) -> (Heap v)
+fromList []     _ = Empty
+fromList [v]    f = Heap f v Empty Empty
+fromList (v:vs) f = insert (fromList vs f) v f
 
 ---------------------------------------------------------
 -------------------------TESTING-------------------------
 ---------------------------------------------------------
 
-main = print()
+type T = (String,Integer)
+
+f :: T -> T -> Bool
+f (_,n) (_,m) = n < m
+
+t0 :: Bool
+t0 = size (Heap f ("0",0) (Heap f ("1",1) Empty Empty) Empty) == 2
+
+h1 :: Heap T
+h1 = Heap f ("h1",1) Empty Empty
+h2 :: Heap T
+h2 = insert h1 ("h2",2) f
+h3 :: Heap T
+h3 = insert h2 ("h3",-1) f
+h4 :: Heap T
+h4 = insert h3 ("h4",5) f
+h5 :: Heap T
+h5 = insert h4 ("h5",-2) f
+h6 :: Heap T
+h6 = insert h5 ("h5",12) f
+t1 :: Bool
+t1 = isHeap h6
+t2 :: Bool
+t2 = size h6 == 6
+
+h10 :: Heap T
+h10 = fromList [("h10",1),("h11",2),("h12",5),("h13",-24),("h14",0),("h15",-5),("h16",8),("h17",-3),("h18",7),("h19",-24)] f
+t3 :: Bool
+t3 = isHeap h10
+t4 :: Bool
+t4 = size h10 == 10
+
+h20 :: Heap T
+h20 = merge h10 h6 f
+
+--main = print (size h6)
+main = print(t0 && t1 && t2)
