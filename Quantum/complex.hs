dc92ed2686d0a1bf8ffe5c3c570842213797af0b
adleypc <jadley08@gmail.com>
2020-02-14 05:38:46 -0500

    matrix mult

--- Quantum/complex.hs
+++ Quantum/complex.hs
@@ -1,6 +1,9 @@
+import Data.Int
+
 type Complex = (Float,Float)
 type Polar   = (Float,Float)
 
+-- Complex
 zero :: Complex
 zero = (0, 0)
 
@@ -22,6 +25,7 @@ times (x1, y1) (x2, y2) = ((x1 * x2) - (y1 * y2), (x1 * y2) + (y1 * x2))
 neg :: Complex -> Complex
 neg (x,y) = (-x,-y)
 
+-- Vectors
 scale :: Complex -> [Complex] -> [Complex]
 scale c zs = map (times c) zs
 
@@ -36,6 +40,17 @@ zero_vec n = replicate n zero
 
 conjugate_vec :: [Complex] -> [Complex]
 conjugate_vec v = map conjugate v
+
+vec_mul :: [Complex] -> [Complex] -> Complex
+vec_mul [] [] = zero
+vec_mul (v : vs) (w : ws) = add (times v w) (vec_mul vs ws)
+
+-- Matrices
+get_num_rows :: [[Complex]] -> Int
+get_num_rows m = length m
+
+get_num_cols :: [[Complex]] -> Int
+get_num_cols m = length (m !! 0)
 
 add_matrix :: [[Complex]] -> [[Complex]] -> [[Complex]]
 add_matrix (x:xs) (y:ys) = (add_vec x y) : (add_matrix xs ys)
@@ -60,7 +75,7 @@ transpose_helper m num_rows num_cols col = if (col >= num_cols)
                                               else ((get_nth_row_of_transpose m num_rows col 0) : (transpose_helper m num_rows num_cols (col + 1)))
 
 transpose :: [[Complex]] -> [[Complex]]
-transpose m = transpose_helper m (length m) (length (m !! 0)) 0
+transpose m = transpose_helper m (get_num_rows m) (get_num_cols m) 0
 
 adjoint :: [[Complex]] -> [[Complex]]
 adjoint m = transpose (conjugate_matrix m)
@@ -97,8 +112,39 @@ list_nth_roots re_part n get_im_part = (re_part, (get_im_part n)) : (list_nth_ro
 nth_root :: Polar -> Float -> [Polar]
 nth_root (r,t) n = list_nth_roots (r ** (1 / n)) (n - 1) (\k -> ((1 / n) * (t + (2 * pi * k))))
 
+get_row :: [[Complex]] -> Int -> [Complex]
+get_row (a:d) 0 = a
+get_row (a:d) n = get_row d (n - 1)
+
+get_column_helper :: [[Complex]] -> Int -> Int -> Int -> [Complex]
+get_column_helper m col cur_row num_rows = if (cur_row >= num_rows)
+                                              then []
+                                              else (((m !! cur_row) !! col) : (get_column_helper m col (cur_row + 1) num_rows))
+
+get_column :: [[Complex]] -> Int -> [Complex]
+get_column m col = get_column_helper m col 0 (get_num_rows m)
+
+matrix_mult_get_row :: [[Complex]] -> [[Complex]] -> Int -> Int -> Int -> Int -> [Complex]
+matrix_mult_get_row m1 m2 cur_row cur_col max_rows max_cols =
+  if (cur_col >= max_cols)
+     then []
+     else ((vec_mul (get_row m1 cur_row) (get_column m2 cur_col)) : (matrix_mult_get_row m1 m2 cur_row (cur_col + 1) max_rows max_cols))
+
+matrix_mult_helper :: [[Complex]] -> [[Complex]] -> Int -> Int -> Int -> [[Complex]]
+matrix_mult_helper m1 m2 cur_row max_rows max_cols =
+  if (cur_row >= max_rows)
+     then []
+     else ((matrix_mult_get_row m1 m2 cur_row 0 max_rows max_cols) : (matrix_mult_helper m1 m2 (cur_row + 1) max_rows max_cols))
+
+matrix_mult :: [[Complex]] -> [[Complex]] -> [[Complex]]
+matrix_mult m1 m2 = matrix_mult_helper m1 m2 0 (get_num_rows m1) (get_num_cols m2)
+
 -- main = print((scale_matrix (1,2) [[(1,-1),(3,0)],[(2,2),(4,1)]]))
 -- main = print(scale_matrix (0,2) (scale_matrix (1,2) [[(1,-1),(3,0)],[(2,2),(4,1)]]))
 -- main = print(scale_matrix (add (0,2) (1,2)) [[(1,-1),(3,0)],[(2,2),(4,1)]])
 -- main = print(transpose [[(6,-3),(2,12),(0,-19)],[(0,0),(5,2.1),(17,0)],[(1,0),(2,5),(3,-4.5)]])
-main = print(adjoint [[(6,-3),(2,12),(0,-19)],[(0,0),(5,2.1),(17,0)],[(1,0),(2,5),(3,-4.5)]])
+-- main = print(adjoint [[(6,-3),(2,12),(0,-19)],[(0,0),(5,2.1),(17,0)],[(1,0),(2,5),(3,-4.5)]])
+-- main = print (scale (1.5,0) (scale (2,0) [(0,0), (1,1)]))
+-- main = print (scale (3,0) [(0,0), (1,1)])
+-- main = print(get_column [[(6,-3),(2,12),(0,-19)],[(0,0),(5,2.1),(17,0)],[(1,0),(2,5),(3,-4.5)]] 1)
+main = print (matrix_mult [[(3,2),(0,0),(5,-6)],[(1,0),(4,2),(0,1)],[(4,-1),(0,0),(4,0)]] [[(5,0),(2,-1),(6,-4)],[(0,0),(4,5),(2,0)],[(7,-4),(2,7),(0,0)]])
